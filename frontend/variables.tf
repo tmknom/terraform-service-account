177c8b0d5ecfd6564d6cbcf89399e7cae312db40
tmknom <tmk.nom@gmail.com>
2018-12-01 20:41:09 +0900

    Enable destruction to frontend stack

---
+++ frontend/variables.tf
@@ -0,0 +1,5 @@
+variable "enable_destruction" {
+  default     = false
+  type        = "string"
+  description = "WARNING: If true, the ALB and S3 bucket can be destroyed. This variable is used during testing. Be sure to specify false, if production environment."
+}
