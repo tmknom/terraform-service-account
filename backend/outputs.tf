224130f3ff55817f972764ef5784c512c7dc0cec
tmknom <tmk.nom@gmail.com>
2018-12-07 23:11:00 +0900

    Implement ECS Fargate

--- backend/outputs.tf
+++ backend/outputs.tf
@@ -7,3 +7,138 @@ output "ecs_cluster_arn" {
   value       = "${aws_ecs_cluster.default.arn}"
   description = "The Amazon Resource Name (ARN) that identifies the cluster."
 }
+
+output "ecs_service_id" {
+  value       = "${module.ecs_fargate.ecs_service_id}"
+  description = "The Amazon Resource Name (ARN) that identifies the service."
+}
+
+output "ecs_service_name" {
+  value       = "${module.ecs_fargate.ecs_service_name}"
+  description = "The name of the service."
+}
+
+output "ecs_service_cluster" {
+  value       = "${module.ecs_fargate.ecs_service_cluster}"
+  description = "The Amazon Resource Name (ARN) of cluster which the service runs on."
+}
+
+output "ecs_service_iam_role" {
+  value       = "${module.ecs_fargate.ecs_service_iam_role}"
+  description = "The ARN of IAM role used for ELB."
+}
+
+output "ecs_service_desired_count" {
+  value       = "${module.ecs_fargate.ecs_service_desired_count}"
+  description = "The number of instances of the task definition."
+}
+
+output "ecs_service_security_group_id" {
+  value       = "${module.ecs_fargate.security_group_id}"
+  description = "The ID of the ECS Service security group."
+}
+
+output "ecs_service_security_group_arn" {
+  value       = "${module.ecs_fargate.security_group_arn}"
+  description = "The ARN of the ECS Service security group."
+}
+
+output "ecs_service_security_group_vpc_id" {
+  value       = "${module.ecs_fargate.security_group_vpc_id}"
+  description = "The VPC ID of the ECS Service security group."
+}
+
+output "ecs_service_security_group_owner_id" {
+  value       = "${module.ecs_fargate.security_group_owner_id}"
+  description = "The owner ID of the ECS Service security group."
+}
+
+output "ecs_service_security_group_name" {
+  value       = "${module.ecs_fargate.security_group_name}"
+  description = "The name of the ECS Service security group."
+}
+
+output "ecs_service_security_group_description" {
+  value       = "${module.ecs_fargate.security_group_description}"
+  description = "The description of the ECS Service security group."
+}
+
+output "ecs_service_security_group_ingress" {
+  value       = "${module.ecs_fargate.security_group_ingress}"
+  description = "The ingress rules of the ECS Service security group."
+}
+
+output "ecs_service_security_group_egress" {
+  value       = "${module.ecs_fargate.security_group_egress}"
+  description = "The egress rules of the ECS Service security group."
+}
+
+output "ecs_task_definition_arn" {
+  value       = "${module.ecs_fargate.ecs_task_definition_arn}"
+  description = "Full ARN of the Task Definition (including both family and revision)."
+}
+
+output "ecs_task_definition_family" {
+  value       = "${module.ecs_fargate.ecs_task_definition_family}"
+  description = "The family of the Task Definition."
+}
+
+output "ecs_task_definition_revision" {
+  value       = "${module.ecs_fargate.ecs_task_definition_revision}"
+  description = "The revision of the task in a particular family."
+}
+
+output "ecs_task_execution_role_arn" {
+  value       = "${module.ecs_fargate.ecs_task_execution_role_arn}"
+  description = "The Amazon Resource Name (ARN) specifying the ecs task execution."
+}
+
+output "ecs_task_execution_role_create_date" {
+  value       = "${module.ecs_fargate.ecs_task_execution_role_create_date}"
+  description = "The creation date of the ecs task execution."
+}
+
+output "ecs_task_execution_role_unique_id" {
+  value       = "${module.ecs_fargate.ecs_task_execution_role_unique_id}"
+  description = "The stable and unique string identifying the ecs task execution."
+}
+
+output "ecs_task_execution_role_name" {
+  value       = "${module.ecs_fargate.ecs_task_execution_role_name}"
+  description = "The name of the ecs task execution."
+}
+
+output "ecs_task_execution_role_description" {
+  value       = "${module.ecs_fargate.ecs_task_execution_role_description}"
+  description = "The description of the ecs task execution."
+}
+
+output "ecs_task_execution_policy_id" {
+  value       = "${module.ecs_fargate.ecs_task_execution_policy_id}"
+  description = "The ecs task execution policy's ID."
+}
+
+output "ecs_task_execution_policy_arn" {
+  value       = "${module.ecs_fargate.ecs_task_execution_policy_arn}"
+  description = "The ARN assigned by AWS to this ecs task execution policy."
+}
+
+output "ecs_task_execution_policy_description" {
+  value       = "${module.ecs_fargate.ecs_task_execution_policy_description}"
+  description = "The description of the ecs task execution policy."
+}
+
+output "ecs_task_execution_policy_name" {
+  value       = "${module.ecs_fargate.ecs_task_execution_policy_name}"
+  description = "The name of the ecs task execution policy."
+}
+
+output "ecs_task_execution_policy_path" {
+  value       = "${module.ecs_fargate.ecs_task_execution_policy_path}"
+  description = "The path of the ecs task execution policy in IAM."
+}
+
+output "ecs_task_execution_policy_document" {
+  value       = "${module.ecs_fargate.ecs_task_execution_policy_document}"
+  description = "The policy document of the ecs task execution policy."
+}
