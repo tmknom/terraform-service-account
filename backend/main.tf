7bd79d52b6d1284b9a30e91affe0143d510da843
tmknom <tmk.nom@gmail.com>
2019-01-03 16:22:53 +0900

    Update ecs-fargate module version to 1.2.0

--- backend/main.tf
+++ backend/main.tf
@@ -4,7 +4,7 @@ resource "aws_ecs_cluster" "default" {
 }
 
 module "ecs_fargate" {
-  source                    = "git::https://github.com/tmknom/terraform-aws-ecs-fargate.git?ref=tags/1.1.0"
+  source                    = "git::https://github.com/tmknom/terraform-aws-ecs-fargate.git?ref=tags/1.2.0"
   name                      = "app"
   desired_count             = 2
   container_name            = "${local.container_name}"
@@ -14,10 +14,8 @@ module "ecs_fargate" {
   target_group_arn          = "${local.target_group_arn}"
   vpc_id                    = "${local.vpc_id}"
   container_definitions     = "${data.template_file.container_definitions.rendered}"
-  ecs_task_execution_policy = "${data.aws_iam_policy.ecs_task_execution.policy}"
 
   assign_public_ip                  = true
-  health_check_grace_period_seconds = 10
 }
 
 data "template_file" "container_definitions" {
@@ -28,10 +26,6 @@ data "template_file" "container_definitions" {
     container_port = "${local.container_port}"
     nginx_image    = "${module.nginx_ecr.ecr_repository_url}:latest"
   }
-}
-
-data "aws_iam_policy" "ecs_task_execution" {
-  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 module "nginx_ecr" {
